112d89307c5f6912bd5433a23b33081539001ea6
j2sleek <j2sleek@gmail.com>
2022-10-08 06:30:29 +0100

    added more reuseable components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
     projects: Project[];
@@ -8,27 +9,48 @@ interface ProjectListProps {
 
 function ProjectList({ projects }: ProjectListProps) {
    // return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
-   return (
-   // <ul className="row">
-   <div className="row">
-        {projects.map((project) => (
-            //<li key={project.id}>{project.name}</li>
-            <div key={project.id} className="cols-sm">
-                {/* <div className="card">
-                    <img src={project.imageUrl} alt={project.name} />
-                    <section className="section dark">
-                        <h5 className="strong">
-                            <strong>{project.name}</strong>
-                        </h5>
-                        <p>{project.description}</p>
-                        <p>Budget : {project.budget.toLocaleString()}</p>
-                    </section>
-                </div> */}
-                <ProjectCard project={project}></ProjectCard>
-            </div>
-        ))}
-    </div>
-   )
+   const [projectBeingEdited, setProjectBeingEdited] = useState({});
+   const handleEdit = (project: Project) => {
+    //console.log(project);
+    setProjectBeingEdited(project);
+   }
+//    return (
+//    // <ul className="row">
+//    //<div className="row">
+//         {projects.map((project) => (
+//             //<li key={project.id}>{project.name}</li>
+//             <div key={project.id} className="cols-sm">
+//                 {/* <div className="card">
+//                     <img src={project.imageUrl} alt={project.name} />
+//                     <section className="section dark">
+//                         <h5 className="strong">
+//                             <strong>{project.name}</strong>
+//                         </h5>
+//                         <p>{project.description}</p>
+//                         <p>Budget : {project.budget.toLocaleString()}</p>
+//                     </section>
+//                 </div> */}
+//                 <ProjectCard project={project}></ProjectCard>
+//                 <ProjectForm />
+//             {/*</div> */}
+//         ))}
+//     </div>
+//    )
+    const items = projects.map(project => (
+        <div key={project.id} className="cols-sm">
+            {/* <ProjectCard
+                project={project}
+                onEdit={handleEdit}
+            ></ProjectCard>
+            <ProjectForm></ProjectForm> */}
+            {project === projectBeingEdited ? (
+                <ProjectForm />
+            ) : (
+                <ProjectCard project={project} onEdit={handleEdit} />
+            )}
+        </div>
+    ));
+    return <div className="row">{items}</div>
 }
 
 export default ProjectList;
